ee9cf9f31b4cdbc19a3dc68bb6ea3946a20a8276
Srinivas <srinu16it@gmail.com>
2016-10-29 22:27:23 -0500

    create added

--- posts/urls.py
+++ posts/urls.py
@@ -9,10 +9,11 @@ from .views import (
 	post_delete,
 	)
 
+
 urlpatterns = [
 	url(r'^$', post_list, name='list'),
-	url(r'^$', post_create, name='create'),
-    url(r'^create/$', post_create),
+	url(r'^create/$', post_create , name='create'),
+    # url(r'^create/$', post_create),
     url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
     url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
     url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
